51e5034412a66c6ed03fb0b95d19e2da8ca014f4
Felix Geisendörfer <felix@debuggable.com>
2013-10-28 11:30:41 +0100

    Add Parse() example

---
+++ example_test.go
@@ -0,0 +1,25 @@
+package mailstrip
+
+import (
+	"fmt"
+)
+
+func ExampleParse() {
+	text :=
+		`Yeah, that works!
+
+-Bob
+
+On 01/03/11 7:07 PM, Alice wrote:
+> Hi Bob,
+>
+> can I push the latest release later tonight?
+`
+
+	email, err := Parse(text)
+	if err != nil {
+		fmt.Printf("err: %#v\n", err)
+	} else {
+		fmt.Printf("result: %q\n", email.String())
+	}
+}
